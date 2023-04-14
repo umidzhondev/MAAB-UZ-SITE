c01b1a1462be39ad048e1d0ecff079ee454beb02
Umidzhon Karabaev <umidzhonfullstackdev@gmail.com>
2023-04-14 17:46:48 +0600

    Responsive

--- js/swiperjs-visualization.js
+++ js/swiperjs-visualization.js
@@ -1,5 +1,28 @@
 var swiper3 = new Swiper(".use-cases--swiper", {
     slidesPerView: 5,
-
+    autoplay: {
+      delay: 2500,
+      disableOnInteraction: false,
+    },
+    breakpoints: {
+      1400:{
+        slidesPerView:5,
+      },
+      993: {
+        slidesPerView: 4.6,
+      },
+      769: {
+        slidesPerView: 3,
+      },
+      577: {
+        slidesPerView:1.5,
+      },
+      401: {
+        slidesPerView: 1.3,
+      },
+      300: {
+        slidesPerView: 1,
+      },
+    },
   });
   
