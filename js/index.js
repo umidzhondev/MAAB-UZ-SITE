0c5d043865e29688ddd61aee39f24b8c27fdb9d0
otabek-oktamov04 <oktamovotabek19@gmail.com>
2023-06-10 11:38:18 +0500

    added form register

--- js/index.js
+++ js/index.js
@@ -28,16 +28,61 @@ document.querySelector("#nowYear").textContent = new Date().getFullYear();
 
 localStorage.setItem("language", "en");
 
-
 select.value = localStorage.getItem("language");
-select.dispatchEvent(new Event('change'));
+select.dispatchEvent(new Event("change"));
 
-let pagesPath =[
+let pagesPath = [
   "/pages/data-analytics.html",
   "/pages/data-visualization.html",
   "/pages/data-engineering.html",
   "/pages/about.html",
   "/pages/contact.html",
   "/index.html",
-]
+];
 
+const contactForm = document.getElementById("contactForm");
+const submitButton = document.querySelector(".form__button");
+
+contactForm.addEventListener("submit", function (event) {
+  event.preventDefault(); // Prevent the default form submission
+
+  // Disable the submit button to prevent multiple submissions
+  submitButton.disabled = true;
+
+  // Show loading state
+  submitButton.value = "Loading...";
+
+  // Get form data
+  const formData = new FormData(contactForm);
+
+  // Make a POST request to the backend API
+  fetch(contactForm.action, {
+    method: "POST",
+    body: formData,
+  })
+    .then((response) => {
+      // Restore the submit button and remove loading state
+      submitButton.disabled = false;
+      submitButton.value = "Send";
+
+      if (response.ok) {
+        // Show success message
+        alert("Form submitted successfully!");
+        contactForm.reset(); // Clear the form fields
+
+        // Disable form submission after success
+        contactForm.removeEventListener("submit", onSubmit);
+      } else {
+        // Handle errors if the request was not successful
+        alert("An error occurred. Please try again.");
+      }
+    })
+    .catch((error) => {
+      // Restore the submit button and remove loading state
+      submitButton.disabled = false;
+      submitButton.value = "Send";
+
+      // Handle network errors
+      alert("A network error occurred. Please try again.");
+    });
+});
